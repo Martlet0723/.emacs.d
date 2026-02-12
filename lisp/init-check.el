4986878e2d52ff0299ff19500b78c6fbaef028ff
Fjlinqiyuan <www.1487604036@qq.com>
2026-02-12 18:07:22 +0800

    refactor(init-check): enhance flyover configuration with detailed settings

--- lisp/init-check.el
+++ lisp/init-check.el
@@ -51,12 +51,48 @@
 ;; Display Flymake errors with overlays
 (use-package flyover
   :diminish
+  :hook (flymake-mode . flyover-mode)
   :custom
+  ;; Checker settings
   (flyover-checkers '(flymake))
-  (flyover-background-lightness 60)
+  (flyover-levels '(error warning info))
+
+  ;; Appearance
+  (flyover-use-theme-colors t)
+  (flyover-background-lightness 45)
+
+  ;; Text tinting
+  (flyover-text-tint 'lighter)
+  (flyover-text-tint-percent 50)
+
+  ;; Icon tinting (foreground and background)
+  (flyover-icon-tint 'lighter)
+  (flyover-icon-tint-percent 50)
+  (flyover-icon-background-tint 'darker)
   (flyover-icon-background-tint-percent 50)
+
+  ;; Border styles: none, pill, arrow, slant, slant-inv, flames, pixels
+  (flyover-border-style 'flames)
+  (flyover-border-match-icon t)
+
+  ;; Display settings
+  (flyover-hide-checker-name t)
+  (flyover-show-virtual-line t)
+  (flyover-virtual-line-type 'curved-dotted-arrow)
+  (flyover-line-position-offset 1)
+
+  ;; Message wrapping
+  (flyover-wrap-messages t)
+
+  ;; Performance
+  (flyover-debounce-interval 0.2)
+  (flyover-cursor-debounce-interval 0.3)
+
+  ;; Display mode (controls cursor-based visibility)
   (flyover-display-mode 'hide-on-same-line)
-  :hook flymake-mode)
+
+  ;; Completion integration
+  (flyover-hide-during-completion t))
 
 (provide 'init-check)
 
