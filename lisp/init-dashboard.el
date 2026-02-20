ac1a52c9e8185f88681c3e5137ff233ac3c8e659
Fjlinqiyuan <www.1487604036@qq.com>
2026-02-21 03:45:07 +0800

    feat(init-dashboard): set image banner max width to 600

--- lisp/init-dashboard.el
+++ lisp/init-dashboard.el
@@ -79,6 +79,7 @@
     :init
     (setq dashboard-banner-logo-title "CENTAUR EMACS - Enjoy Programming & Writing"
           dashboard-startup-banner (or centaur-logo 'official)
+          dashboard-image-banner-max-width 600
           dashboard-page-separator "\n\f\n"
           dashboard-projects-backend 'project-el
           dashboard-path-style 'truncate-middle
