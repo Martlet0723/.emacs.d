3a2cf29aba67348cd22d5fbf99737595d5a29045
Fjlinqiyuan <www.1487604036@qq.com>
2026-03-08 17:17:36 +0800

    refactor(window): disable switch-window

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -205,17 +205,17 @@
     (advice-add #'keyboard-quit :before #'popper-close-window-hack)))
 
 ;; Window switch with switch-window (avoid EAF遮挡)
-(use-package switch-window
-  :ensure t
-  :bind (("C-c o" . switch-window)
-         ("C-c p" . switch-window-then-delete))
-  :config
-  ;; 使用数字编号
-  (setq switch-window-shortcut-style 'numbers)
-  ;; 多窗口时显示提示
-  (setq switch-window-threshold 2)
-  ;; 提示样式
-  (setq switch-window-caption nil))
+;; (use-package switch-window
+;;   :ensure t
+;;   :bind (("C-c o" . switch-window)
+;;          ("C-c p" . switch-window-then-delete))
+;;   :config
+;;   ;; 使用数字编号
+;;   (setq switch-window-shortcut-style 'numbers)
+;;   ;; 多窗口时显示提示
+;;   (setq switch-window-threshold 2)
+;;   ;; 提示样式
+;;   (setq switch-window-caption nil))
 
 (provide 'init-window)
 
